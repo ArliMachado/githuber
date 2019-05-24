3df0a12a94ddd000010b210791bbea5a00df344a
Arli Machado <arli.machado@b2wdigital.com>
2019-05-24 17:52:24 -0300

    Criando Abas de navegações de repositorios e organizações

--- src/pages/Organization/index.js
+++ src/pages/Organization/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { View } from 'react-native';
+import Header from '~/components/Header';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const Organization = () => <View />;
+const Organizations = () => (
+  <View>
+    <Header title="Organizações" />
+  </View>
+);
 
-export default Organization;
+const TabIcon = ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />;
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Organizations.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
+
+export default Organizations;
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { View } from 'react-native';
 import Header from '~/components/Header';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Repositories = () => (
   <View>
     <Header title="Repositórios" />
   </View>
 );
+
+const TabIcon = ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />;
+
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Repositories.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Repositories;

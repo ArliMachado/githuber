09e77320488237a3a00e3b457dfb225e02bb2e8b
Arli Machado <arli.machado@b2wdigital.com>
2019-05-24 16:00:45 -0300

    Criando componente de header

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import '~/config/DevToolsConfig';
 import '~/config/ReactotronConfig';
 
-import React from 'react';
-import Routes from '~/routes';
+import React, { Component } from 'react';
+import createNavigator from '~/routes';
+import AsyncStorage from '@react-native-community/async-storage';
 
-const App = () => <Routes />;
+export default class App extends Component {
+  state = {
+    userChecked: false,
+    userLogged: false,
+  };
 
-export default App;
+  async componentDidMount() {
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    this.setState({
+      userChecked: true,
+      userLogged: !!username,
+    });
+  }
+
+  render() {
+    const { userChecked, userLogged } = this.state;
+
+    if (!userChecked) return null;
+
+    const Routes = createNavigator(userLogged);
+
+    return <Routes />;
+  }
+}
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import { View } from 'react-native';
+import Header from '~/components/Header';
 
-// import { Container } from './styles';
-
-const Repositories = () => <View />;
+const Repositories = () => (
+  <View>
+    <Header title="Repositórios" />
+  </View>
+);
 
 export default Repositories;

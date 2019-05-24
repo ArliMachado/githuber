4150784f0edd147dcd2963c7c37538096e32478c
Arli Machado <arli.machado@b2wdigital.com>
2019-05-24 16:33:04 -0300

    Efetuando logout na aplicação

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import {
   View,
@@ -14,6 +15,12 @@ import AsyncStorage from '@react-native-community/async-storage';
 import styles from './styles';
 
 export default class Welcome extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
   state = {
     username: '',
     loading: false,
